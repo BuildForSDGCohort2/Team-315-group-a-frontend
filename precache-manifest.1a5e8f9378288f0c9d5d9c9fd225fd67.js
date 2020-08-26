f8697d9e0d83eb4c9eb035cdf29cabda13baaa3a
miami78 <miamilarry78@gmail.com>
2020-08-26 15:06:19 +0300

    Updates

---
+++ precache-manifest.1a5e8f9378288f0c9d5d9c9fd225fd67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba64d4daae1080e3db2809df1a79b61d",
+    "url": "/converge-frontend/index.html"
+  },
+  {
+    "revision": "ccddfc9e5dbb1d1bfc5a",
+    "url": "/converge-frontend/static/css/main.b14a6dc3.chunk.css"
+  },
+  {
+    "revision": "b2657d679645e94b44d1",
+    "url": "/converge-frontend/static/js/2.9d6f4ed0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/converge-frontend/static/js/2.9d6f4ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccddfc9e5dbb1d1bfc5a",
+    "url": "/converge-frontend/static/js/main.a19e57b4.chunk.js"
+  },
+  {
+    "revision": "3f77cc59681c42665600",
+    "url": "/converge-frontend/static/js/runtime-main.442b1c0c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7Bconverge-frontend%7D/precache-manifest.a5840c7a57fcee441f1c6ecefc293ce0.js"
+  "/converge-frontend/precache-manifest.1a5e8f9378288f0c9d5d9c9fd225fd67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Bconverge-frontend%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/converge-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

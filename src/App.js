bb22d1d8677b1a84a81b40e4a20aabb5c038b86a
Arnold Kibira <arnold.ril3y@gmail.com>
2020-09-05 13:48:56 +0300

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 import Header from "./components/Header/header.component";
 import Landing from "./components/LandingPage/Landing";
 function App() {
@@ -7,6 +8,17 @@ function App() {
       <Header />
       <Landing />
     </React.Fragment>
+=======
+// import Header from "./components/Header/header.component";
+import Login from "./components/Login/login";
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <Login />
+    </>
+>>>>>>> track untracked files
   );
 }
 
